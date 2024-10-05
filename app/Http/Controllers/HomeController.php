9c61c29f8190636aa88e7249fab14e49f154ec2f
BasmalaBrham <basmala.brham@gmail.com>
2024-10-05 22:18:01 +0300

    handle the home page Search feature

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,13 +11,15 @@ class HomeController extends Controller
     // to show the home page
     public function index(){
         $categories = Category::where('status', '1')->orderBy('name', 'ASC')->take(8)->get();
+        $newCategories = Category::where('status', '1')->orderBy('name', 'ASC')->get();
         $featuredJobs = Job::where('isFeatured', '1')->where('status', '1')->with('jobType')->orderBy('created_at', 'DESC')->take(6)->get();
         $latestJobs = Job::where('status', '1')->with('jobType')->orderBy('created_at', 'DESC')->take(6)->get();
 
         return view("front.home", [
             'categories' => $categories,
             'featuredJobs' => $featuredJobs,
-            'latestJobs' => $latestJobs
+            'latestJobs' => $latestJobs,
+            'newCategories'=>$newCategories
         ]);
     }
 
